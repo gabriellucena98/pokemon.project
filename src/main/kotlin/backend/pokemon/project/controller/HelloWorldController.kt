0d4f9afa0cf8a3cd6c163f1799267a2be8cba158
gabriellucena98 <gabrieltim1@hotmail.com>
2023-04-04 11:51:58 -0300

    helloWorld commit

---
+++ src/main/kotlin/backend/pokemon/project/controller/HelloWorldController.kt
@@ -0,0 +1,13 @@
+package backend.pokemon.project.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/hello")
+    fun helloWorld(): String {
+        return "Hello World, guys!!"
+    }
+}
